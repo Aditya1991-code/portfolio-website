343e5467446a6e1d6ebc3e06231cea2c02ef0b73
Aditya1991-code <85364983+Aditya1991-code@users.noreply.github.com>
2024-05-15 13:33:14 -0500

    Update portfolio.js

--- Adi Akkapeddi Portfolio/js/portfolio.js
+++ Adi Akkapeddi Portfolio/js/portfolio.js
@@ -1,1 +1,9 @@
-console.log ("welcome to my Adi Akkapeddi's portfolio")
+console.log("Hi welcome to Adi Akkapeddi's profile")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'adinavtoggle') {
+      x.className += 'responsive';
+    } else {
+      x.className = 'adinavtoggle';
+    }
+  }

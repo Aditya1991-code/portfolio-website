b72f6c8703044f1efa47dfc8e5f097df4db78b44
Aditya1991-code <85364983+Aditya1991-code@users.noreply.github.com>
2024-05-17 13:53:35 -0500

    Delete Adi Akkapeddi Portfolio directory

--- Adi Akkapeddi Portfolio/js/Adiprofile.js
+++
@@ -1,9 +0,0 @@
-console.log("Hi welcome to Adi Akkapeddi's profile")
-function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'adinavtoggle') {
-      x.className += 'responsive';
-    } else {
-      x.className = 'adinavtoggle';
-    }
-  }

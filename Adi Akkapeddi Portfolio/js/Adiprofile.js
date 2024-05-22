984072fdb0e4d0c709b8c810400681460d979d7c
Aditya1991-code <85364983+Aditya1991-code@users.noreply.github.com>
2024-05-22 14:24:40 -0500

    Update Adiprofile.js

--- Adi Akkapeddi Portfolio/js/Adiprofile.js
+++ Adi Akkapeddi Portfolio/js/Adiprofile.js
@@ -7,3 +7,23 @@ function menuToggle() {
       x.className = 'adinavtoggle';
     }
   }
+  
+  let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
